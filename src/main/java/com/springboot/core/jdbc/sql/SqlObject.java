b23c742ad8326758771617dce1dcada52431d1c6
masonleung2016 <masonleung@163.com>
2020-01-04 17:48:00 +0800

    Create SqlObject.java

---
+++ src/main/java/com/springboot/core/jdbc/sql/SqlObject.java
@@ -0,0 +1,16 @@
+package com.springboot.core.jdbc.sql;
+
+import java.util.List;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/2 17:24
+ * @Package: com.springboot.core.jdbc.sql
+ */
+
+public class SqlObject {
+
+    public String sql;
+    public List<Object> params;
+
+}
