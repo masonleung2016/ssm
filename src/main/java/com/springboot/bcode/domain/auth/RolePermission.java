200d47deda5b1f3d47a95b449d4f58dbbc37266f
masonleung2016 <masonleung@163.com>
2020-01-04 09:29:36 +0800

    Create RolePermission.java

---
+++ src/main/java/com/springboot/bcode/domain/auth/RolePermission.java
@@ -0,0 +1,38 @@
+package com.springboot.bcode.domain.auth;
+
+import com.springboot.core.jdbc.annotation.Columns;
+import com.springboot.core.jdbc.annotation.Tables;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/2 16:47
+ * @Package: com.springboot.bcode.domain.auth
+ */
+
+@Tables(table = "t_web_role_permission")
+public class RolePermission {
+
+    @Columns(column = "role_id")
+    private Integer roleId;
+    @Columns(column = "perm_id")
+    private Integer permId;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getPermId() {
+        return permId;
+    }
+
+    public void setPermId(Integer permId) {
+        this.permId = permId;
+    }
+
+
+
+}
