bcfb2c981ce389398a43757c2f7c93efb3f9fbb7
masonleung2016 <masonleung@163.com>
2020-01-04 18:01:11 +0800

    Create BaseRest.java

---
+++ src/main/java/com/springboot/core/web/mvc/BaseRest.java
@@ -0,0 +1,16 @@
+package com.springboot.core.web.mvc;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/2 17:53
+ * @Package: com.springboot.core.web.mvc
+ */
+
+public class BaseRest {
+    //ok
+    public static final int CODE_200 = 20000;
+    //error
+    public static final int CODE_500 = 50000;
+
+
+}
