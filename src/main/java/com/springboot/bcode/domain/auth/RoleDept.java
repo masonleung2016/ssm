01276e267dc571c074b14cb77fefbeef38b39dfa
masonleung2016 <masonleung@163.com>
2020-01-04 09:29:19 +0800

    Create RoleDept.java

---
+++ src/main/java/com/springboot/bcode/domain/auth/RoleDept.java
@@ -0,0 +1,38 @@
+package com.springboot.bcode.domain.auth;
+
+import com.springboot.core.jdbc.annotation.Columns;
+import com.springboot.core.jdbc.annotation.Tables;
+
+/**
+ * 数据权限表
+ *
+ * @Author: LCF
+ * @Date: 2020/1/2 16:45
+ * @Package: com.springboot.bcode.domain.auth
+ */
+
+@Tables(table = "t_web_role_dept")
+public class RoleDept {
+
+    @Columns(column = "role_id")
+    private Integer roleId;
+    @Columns(column = "dept_id")
+    private Integer deptId;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Integer deptId) {
+        this.deptId = deptId;
+    }
+
+}
