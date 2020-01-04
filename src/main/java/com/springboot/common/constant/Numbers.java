eecb062359ddee543fe55b638e0805b9b22e9e0e
masonleung2016 <masonleung@163.com>
2020-01-04 17:37:41 +0800

    Create Numbers.java

---
+++ src/main/java/com/springboot/common/constant/Numbers.java
@@ -0,0 +1,19 @@
+package com.springboot.common.constant;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/2 17:10
+ * @Package: com.springboot.common.constant
+ */
+
+public class Numbers {
+
+    public static final int NEG_NUM_1 = -1;
+    public static final int NUM_1 = 1;
+    public static final int NUM_2 = 2;
+    public static final int NUM_3 = 3;
+    public static final int NUM_4 = 4;
+    public static final int NUM_5 = 5;
+    public static final int NUM_30 = 30;
+
+}
