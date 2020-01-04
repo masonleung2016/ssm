fc688ea6995f36c2af47d7207ded0def6a97ec27
masonleung2016 <masonleung@163.com>
2020-01-04 17:41:48 +0800

    Create HttpUtils.java

---
+++ src/main/java/com/springboot/common/utils/HttpUtils.java
@@ -0,0 +1,19 @@
+package com.springboot.common.utils;
+
+import com.springboot.bcode.domain.auth.Department;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/3 14:48
+ * @Package: com.springboot.common.utils
+ */
+
+public class HttpUtils {
+    public static Department getSession() {
+        return null;
+    }
+
+    public static Object getRequest() {
+        return null;
+    }
+}
