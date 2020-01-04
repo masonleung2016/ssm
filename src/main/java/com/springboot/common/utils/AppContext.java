9b0f718f25ddc6956a737daa7f5e79c03919250d
masonleung2016 <masonleung@163.com>
2020-01-04 17:40:57 +0800

    Create AppContext.java

---
+++ src/main/java/com/springboot/common/utils/AppContext.java
@@ -0,0 +1,25 @@
+package com.springboot.common.utils;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/2 17:14
+ * @Package: com.springboot.common.utils
+ */
+
+public class AppContext {
+
+    public static final String TOKEN = "LinkAdmin-Token";
+    // ok
+    public static final int CODE_20000 = 20000;
+    // error
+    public static final int CODE_50000 = 50000;
+    // Illegal token
+    public static final int CODE_50001 = 50001;
+    // no permission
+    public static final int CODE_50002 = 50002;
+    // DemoSystem
+    public static final int CODE_50003 = 50003;
+    //service reject request
+    public static final int CODE_50004 = 50004;
+
+}
