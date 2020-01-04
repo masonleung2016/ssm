2fc25671135f366185246db655e249343edc62c5
masonleung2016 <masonleung@163.com>
2020-01-04 18:00:59 +0800

    Create BaseEntity.java

---
+++ src/main/java/com/springboot/core/web/mvc/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.springboot.core.web.mvc;
+
+import java.io.Serializable;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/2 17:51
+ * @Package: com.springboot.core.web.mvc
+ */
+
+public class BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = -4194990642350165590L;
+
+}
