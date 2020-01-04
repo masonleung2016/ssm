6c2427c197ecbc591b058f93ab2f71739f2d1b95
masonleung2016 <masonleung@163.com>
2020-01-04 17:42:37 +0800

    Create UUIDUtils.java

---
+++ src/main/java/com/springboot/common/utils/UUIDUtils.java
@@ -0,0 +1,12 @@
+package com.springboot.common.utils;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/3 14:49
+ * @Package: com.springboot.common.utils
+ */
+
+public class UUIDUtils {
+    public static char[] generateUUID() {
+    }
+}
