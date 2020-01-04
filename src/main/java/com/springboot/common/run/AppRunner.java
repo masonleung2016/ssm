81c82c9d7cf520f6da7c114eec494510936e7f86
masonleung2016 <masonleung@163.com>
2020-01-04 17:40:17 +0800

    Create AppRunner.java

---
+++ src/main/java/com/springboot/common/run/AppRunner.java
@@ -0,0 +1,23 @@
+package com.springboot.common.run;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * 服务启动后执行的代码
+ *
+ * @Author: LCF
+ * @Date: 2020/1/2 17:12
+ * @Package: com.springboot.common.run
+ */
+
+@Component
+public class AppRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+
+}
