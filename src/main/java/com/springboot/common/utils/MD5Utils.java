0c67b259b00057cd9297cceedba9baaf10d31a5c
masonleung2016 <masonleung@163.com>
2020-01-04 17:42:17 +0800

    Create MD5Utils.java

---
+++ src/main/java/com/springboot/common/utils/MD5Utils.java
@@ -0,0 +1,13 @@
+package com.springboot.common.utils;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/3 14:49
+ * @Package: com.springboot.common.utils
+ */
+
+public class MD5Utils {
+    public static String getMD5AndBase64(String toString) {
+        return null;
+    }
+}
