95aa722659ff68f29d60f3d9b2d4aaa71370ac02
masonleung2016 <masonleung@163.com>
2020-01-04 17:49:33 +0800

    Create AbstractJdbcSupport.java

---
+++ src/main/java/com/springboot/core/jdbc/support/AbstractJdbcSupport.java
@@ -0,0 +1,10 @@
+package com.springboot.core.jdbc.support;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/3 14:54
+ * @Package: com.springboot.core.jdbc.support
+ */
+
+public class AbstractJdbcSupport {
+}
