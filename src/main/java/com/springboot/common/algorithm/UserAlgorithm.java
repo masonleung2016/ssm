113140fb0d3d3a4f0e176a3916fa0d1d0ec33905
masonleung2016 <masonleung@163.com>
2020-01-04 17:35:31 +0800

    Create UserAlgorithm.java

---
+++ src/main/java/com/springboot/common/algorithm/UserAlgorithm.java
@@ -0,0 +1,12 @@
+package com.springboot.common.algorithm;
+
+/**
+ * 用户类相关算法
+ *
+ * @Author: LCF
+ * @Date: 2020/1/2 17:07
+ * @Package: com.springboot.common.algorithm
+ */
+
+public class UserAlgorithm {
+}
