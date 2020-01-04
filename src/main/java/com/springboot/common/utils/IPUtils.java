62ad8453e347c53a5b61e2c77622d9a92521e478
masonleung2016 <masonleung@163.com>
2020-01-04 17:42:03 +0800

    Create IPUtils.java

---
+++ src/main/java/com/springboot/common/utils/IPUtils.java
@@ -0,0 +1,13 @@
+package com.springboot.common.utils;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/3 14:49
+ * @Package: com.springboot.common.utils
+ */
+
+public class IPUtils {
+    public static char[] getIpAddr(Object request) {
+        return new char[0];
+    }
+}
